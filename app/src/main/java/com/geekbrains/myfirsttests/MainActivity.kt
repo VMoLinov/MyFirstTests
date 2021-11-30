763856cc582e0480cccd4d5aa10fef083b1fb01d
VMolinov <molinov@list.ru>
2021-11-30 14:27:48 +0300

    Done

--- app/src/main/java/com/geekbrains/myfirsttests/MainActivity.kt
+++ app/src/main/java/com/geekbrains/myfirsttests/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val emailValidator = EmailValidator()
+    private val numberChecker = NumberChecker()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +18,36 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             emailInput.addTextChangedListener(emailValidator)
+            numberInput.addTextChangedListener(numberChecker)
 
             saveButton.setOnClickListener {
-                if (emailValidator.isValid) {
-                    Toast.makeText(this@MainActivity, getString(R.string.valid_email), Toast.LENGTH_SHORT).show()
-                } else {
-                    val errorEmail = getString(R.string.invalid_email)
-                    emailInput.error = errorEmail
-                    Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
+                when {
+                    emailInput.text.toString() != "" -> {
+                        emailValidatorFun()
+                    }
+                    numberInput.text.toString() != "" -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            numberChecker.value.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
+        }
+    }
+
+    private fun emailValidatorFun() {
+        if (emailValidator.isValid) {
+            Toast.makeText(
+                this@MainActivity,
+                getString(R.string.valid_email),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            val errorEmail = getString(R.string.invalid_email)
+            binding.emailInput.error = errorEmail
+            Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
         }
     }
 }
